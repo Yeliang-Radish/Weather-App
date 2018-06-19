848bab729338723f62126ba2c53fc372019f3b39
Yeliang Shou <yeliang.shou@gmail.com>
2018-06-19 19:27:04 -0400

    Finished the basics of the weather app
    The weather app now querys google's maps api and the darksky api to
    get the latitude and longitude, and use that to get the current
    temperature.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,7 @@
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs
     .options({
@@ -19,22 +20,14 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if (errorMessage) {
         console.log(errorMessage);
     } else {
-        console.log(JSON.stringify(results, undefined, 4));
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log (errorMessage);
+            } else {
+                console.log (`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
+            }
+        });
     }
 });
 
-// c8d597ab1284821432820ddca9ab64d5
-
-// const request = require('request');
-
-// request ({
-//     uri: 'https://api.forecast.io/forecast/c8d597ab1284821432820ddca9ab64d5/45.323507,-75.91992809999999',
-//     json: true,
-//     proxy: 'http://19.12.1.140:83'
-// }, (error, response, body) => {
-//     if (!error && response.statusCode === 200) {
-//         console.log(body.currently.temperature);
-//     } else {
-//         console.log('Unable to fetch weather.');
-//     }
-// });
